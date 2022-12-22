b722730f9d4c7d13fc2b3cd72186032136cc83fc
olexa <olexakljus@gmail.com>
2022-12-22 11:20:16 +0200

    lecture 1

---
+++ Lecture 1 venv.py
@@ -0,0 +1,8 @@
+# віртуальне оточення інсталювати, створити, активувати
+
+# python -m pip install virtualenv
+# python -m venv venv
+# venv\Scripts\activate.bat
+
+# для клонування проекту з гіта перейти в папку куди буде клон і
+# git clone посилання на репозиторій
